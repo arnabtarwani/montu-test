b8aa81949c37bbdf2cf04f0c822d71538ea01b23
arnabtarwani <arnab.tarwani@gmail.com>
2024-12-18 05:25:26 +0000

    fixed base.ts

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,13 +1,12 @@
 import { GIPHY_API_KEY, GIPHY_BASE_URL } from "../lib/constants"
 
-export const APIWrapper = async ({ endpoint, method, payload, params }: {
+export const APIWrapper = async ({ endpoint, method, payload, params, abortController }: {
     endpoint: string,
     method: string,
     payload?: any,
     params?: Record<string, any>,
+    abortController?: AbortController
 }) => {
-    const abortController = new AbortController()
-
     try {
         const queryParams = new URLSearchParams(params)
 
@@ -29,7 +28,5 @@ export const APIWrapper = async ({ endpoint, method, payload, params }: {
         return data
     } catch (error) {
         throw error;
-    } finally {
-        return abortController.abort();
     }
 }
